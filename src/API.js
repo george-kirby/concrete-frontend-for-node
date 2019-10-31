3c6a93f1941d3550384a6eff39b2d2c97604b52f
george-kirby <georgeskirby@btinternet.com>
2019-10-31 16:33:12 +0000

    created api getUser

--- src/API.js
+++ src/API.js
@@ -1,9 +1,11 @@
 const BASE_URL = "http://localhost:3000/"
-const USERS_URL = `${BASE_URL}users`
-const PROJECTS_URL = `${BASE_URL}projects`
-const TASKS_URL = `${BASE_URL}tasks`
-const STEPS_URL = `${BASE_URL}steps`
+const USERS_URL = `${BASE_URL}users/`
+const PROJECTS_URL = `${BASE_URL}projects/`
+const TASKS_URL = `${BASE_URL}tasks/`
+const STEPS_URL = `${BASE_URL}steps/`
 
-const getUserProjects = () => {
-    
+const getUser = userId => {
+    return fetch(USERS_URL+userId).then(resp => resp.json())
 }
+
+export default { getUser }
