b424e52dacfea924bb33652e8767dc2b3d29cae2
george-kirby <georgeskirby@btinternet.com>
2019-11-01 13:33:07 +0000

    API login function

--- src/adapters/API.js
+++ src/adapters/API.js
@@ -3,9 +3,27 @@ const USERS_URL = `${BASE_URL}users/`
 const PROJECTS_URL = `${BASE_URL}projects/`
 const TASKS_URL = `${BASE_URL}tasks/`
 const STEPS_URL = `${BASE_URL}steps/`
+const LOGIN_URL = `${BASE_URL}login/`
+
+const jsonHeaders = (more = {}) => {
+    return {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      ...more
+    }
+  }
+
+const login = credentials => {
+    return fetch(LOGIN_URL, {
+        method: "POST",
+        headers: jsonHeaders(),
+        body: {user: credentials}
+    })
+    .then(resp => resp.json())
+}
 
 const getUser = userId => {
     return fetch(USERS_URL+userId).then(resp => resp.json())
 }
 
-export default { getUser }
+export default { getUser, login }
