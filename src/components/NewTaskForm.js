83afefc9620206b7a936956bf3e38ab1af5a19bc
george-kirby <georgeskirby@btinternet.com>
2019-11-02 15:06:13 +0000

    dynamic options for casualDate

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,10 +1,10 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import "../stylesheets/NewTaskForm.css"
 
 const NewTaskForm = () => {
-  const [casualDate, setCasualDate] = useState("tonight")
+  const [casualDate, setCasualDate] = useState(null)
   const [calendarDate, setCalendarDate] = useState(null)
-  const [casualTime, setCasualTime] = useState(null)
+  const [casualTime, setCasualTime] = useState("morning")
   const [preciseTime, setPreciseTime] = useState(null)
 
   const handleSubmit = event => {
@@ -12,73 +12,130 @@ const NewTaskForm = () => {
     console.log("form submitted!")
   }
 
-  const handleCasualDateChange = event => setCasualDate(event.target.value)
+  const currentDateTime = new Date()
+
+  useEffect(() => {
+    setCasualDate(casualDateOptions()[0])
+  }, []);
+
+  const handleCasualDateChange = event => {
+    setCasualDate(event.target.value)
+    //   if (event.target.value)
+  }
   const handleCalendarDateChange = event => setCalendarDate(event.target.value)
   const handleCasualTimeChange = event => setCasualTime(event.target.value)
   const handlePreciseTimeChange = event => setPreciseTime(event.target.value)
 
+  const casualDateOptions = () => {
+    let extras = []
+    if (currentDateTime.getHours() < 16) {
+        extras = [{ value: "thisAfternoon", display: "This afternoon" }, ...extras]
+    } 
+    if (currentDateTime.getHours() < 12) {
+        extras = [{ value: "thisMorning", display: "This morning" }, ...extras]
+    }
+    // const dayAfterTomorrow = {
+    //     value: , display: `${}`
+    // }
+    return [
+      ...extras,
+      { value: "tonight", display: "Tonight" },
+      { value: "tomorrow", display: "Tomorrow" },
+    //   dayAfterTomorrow,
+      { value: "later", display: "Later" }
+    ]
+  }
+
+  const casualTimeOptions = [
+    { value: "morning", display: "Morning" },
+    { value: "afternoon", display: "Afternoon" },
+    { value: "evening", display: "Evening" },
+    { value: "preciseTime", display: "Precise time" }
+  ]
+
   return (
     <div>
-      <form action="" onSubmit={handleSubmit}></form>
-      <input name="title" type="text" placeholder="Task name..." />
-      <br />
-      <label>
-        What's a concrete first step?
+      <form action="" onSubmit={handleSubmit}>
+        <input name="title" type="text" placeholder="Task name..." required />
         <br />
-        <input
-          name="step_act"
-          type="text"
-          placeholder={`eg sit at desk with laptop`}
-        />
-      </label>
-      <br />
-      <div>
-          <select
-            name="casualDate"
-            onChange={handleCasualDateChange}
-            value={casualDate}
-          >
-            {/* dynamically calculate if this morning/this afternoon would be appropriate to show */}
-            <option value="tonight">Tonight</option>
-            <option value="tomorrow">Tomorrow</option>
-            {/* <option value="tomorrow">Day after tomorrow</option> for day after tomorrow, dynamically calculated */}
-            <option value="later">Later</option>
-            {/* <input type="date">Date</input> */}
-          </select>
+        <label>
+          What's a concrete first step?
+          <br />
           <input
-            className={
-              casualDate === "later" ? "visible" : `hidden`
-            }
-            type="date"
-            name="calendar"
-            onChange={handleCalendarDateChange}
-          />
-      </div>
-      <div>
-          <select
-            className={casualDate !== "tonight" ? "visible" : `hidden`}
-            name="timeDetail"
+            name="step_act"
             type="text"
-            onChange={handleCasualTimeChange}
-          >
-            <option value="" disabled selected>
-              Choose a time
-            </option>
-            <option value="morning">Morning</option>
-            <option value="afternoon">Afternoon</option>
-            <option value="evening">Evening</option>
-            <option value="preciseTime">Precise time</option>
-          </select>
-          <input
-            className={
-              casualTime === "preciseTime" ? "visible" : `hidden`
-            }
-            type="time"
-            name="preciseTime"
-            onChange={handlePreciseTimeChange}
+            placeholder={`eg sit at desk with laptop`}
+            required
           />
-      </div>
-      <input name="cue" type="text" placeholder="" />
+        </label>
+        <br />
+        <label>
+          When will you do this? <br />
+          <div>
+            <select
+              name="casualDate"
+              onChange={handleCasualDateChange}
+              value={casualDate}
+              required
+            >
+              {casualDateOptions().map(option => {
+                return (
+                  <option key={option.value} value={option.value}>
+                    {option.display}
+                  </option>
+                )
+              })}
+              {/* dynamically calculate if this morning/this afternoon would be appropriate to show */}
+              {/* <option value="tomorrow">Day after tomorrow</option> for day after tomorrow, dynamically calculated */}
+              {/* <input type="date">Date</input> */}
+            </select>
+            <input
+              className={casualDate === "later" ? "visible" : `hidden`}
+              type="date"
+              name="calendar"
+              onChange={handleCalendarDateChange}
+              required={casualDate === "later"}
+            />
+          </div>
+          <div>
+            <select
+              className={casualDate !== "tonight" ? "visible" : `hidden`}
+              name="timeDetail"
+              type="text"
+              onChange={handleCasualTimeChange}
+              value={casualTime}
+              required={casualDate !== "tonight"}
+            >
+              {casualTimeOptions.map(option => {
+                return (
+                  <option key={option.value} value={option.value}>
+                    {option.display}
+                  </option>
+                )
+              })}
+            </select>
+            <input
+              className={casualTime === "preciseTime" ? "visible" : `hidden`}
+              type="time"
+              name="preciseTime"
+              onChange={handlePreciseTimeChange}
+              required={casualTime === "preciseTime"}
+            />
+          </div>
+        </label>
+        <label>
+          Task cue: <br />
+          <input
+            name="cue"
+            type="text"
+            placeholder="eg after dinner"
+            required
+          />
+          {/* ^ dynamically set placeholder based on time choice eg morning -> after breakfast, evening -> after dinner */}
+        </label>
+        <br />
+        <input type="submit" value="Create Task" />
+      </form>
     </div>
   )
 }
