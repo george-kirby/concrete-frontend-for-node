d7c208484c80e17a7838b5b088d6303c5d5894c3
george-kirby <georgeskirby@btinternet.com>
2019-11-05 16:47:50 +0000

    link to project show

--- src/components/TaskCard.js
+++ src/components/TaskCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom'
+import { useHistory, Link } from 'react-router-dom'
 import '../stylesheets/TaskCard.css'
 import API from '../adapters/API'
 import UpdateUserObject from "../helpers/UpdateUserObject"
@@ -24,7 +24,7 @@ const TaskCard = ({task, setCurrentUser, currentUser}) => {
         <div className="task-card">
             <div className="task-details">
                 <h4>{task.title}</h4>
-                <p>{task.project.title !== "" && `📌 ${task.project.title}`}</p>
+                <p>{task.project.title !== "" && <span>📌 <Link to={`/projects/${task.project.id}`}>{task.project.title}</Link> </span>}</p>
                 <p>🕑 this {task.display_time}, {task.cue}</p>
             </div>
             <div className="task-actions">
