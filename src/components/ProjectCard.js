99186de60c42dc40f9992ae43db3018cfe88f3ee
george-kirby <georgeskirby@btinternet.com>
2019-11-05 13:06:04 +0000

    progress bar on projectCard

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,23 +1,35 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom'
-import '../stylesheets/ProjectCard.css'
+import React from "react"
+import { useHistory } from "react-router-dom"
+import "../stylesheets/ProjectCard.css"
+import Sorting from "../helpers/Sorting"
 
-const ProjectCard = ({project, handleUpdateToggle}) => {
+const ProjectCard = ({ project, handleUpdateToggle }) => {
+  const history = useHistory()
 
-    const history = useHistory()
+  const progress =
+    ((project.tasks.length - Sorting.incompleteTasks(project.tasks).length) /
+      project.tasks.length) *
+    100
 
-    return (
-        <div className="project-card"  onClick={() => history.push(`/projects/${project.id}`)}>
-            <h4>📌 {project.title}</h4>
-            progress bar...
-            <br/>
-            {project.tasks.map(task => {
-                return <div key={task.id}>
-                    {task.title} 🕑 {task.actual_time}
-                </div>
-            })}
-        </div>
-    );
+  return (
+    <div
+      className="project-card"
+      onClick={() => history.push(`/projects/${project.id}`)}
+    >
+      <h4>📌 {project.title}</h4>
+      <div id="progress-bar-container">
+        <div id="progress-bar" style={{ width: `${progress - 1}%` }}></div>
+      </div>
+      <br />
+      {project.tasks.map(task => {
+        return (
+          <div key={task.id}>
+            {task.title} 🕑 {task.actual_time}
+          </div>
+        )
+      })}
+    </div>
+  )
 }
 
-export default ProjectCard;
+export default ProjectCard
