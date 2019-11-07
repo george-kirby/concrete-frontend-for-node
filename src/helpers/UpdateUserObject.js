d0b53cb4089b20129793291f08ada1575949a01a
george-kirby <georgeskirby@btinternet.com>
2019-11-07 17:35:42 +0000

    removed defunct functions

--- src/helpers/UpdateUserObject.js
+++ src/helpers/UpdateUserObject.js
@@ -1,18 +1,3 @@
-const patchedStep = (responseStep, currentUser) => {
-  let newProjects = [...currentUser.projects]
-  let projectIndex = newProjects.findIndex(
-    p => p.id === responseStep.task.project.id
-  )
-  let newTasks = [...newProjects[projectIndex].tasks]
-  let taskIndex = newTasks.findIndex(t => t.id === responseStep.task.id)
-  let newSteps = [...newTasks[taskIndex].steps]
-  let stepIndex = newSteps.findIndex(s => s.id === responseStep.id)
-
-  newSteps[stepIndex] = responseStep
-  newTasks[taskIndex].steps = newSteps
-  newProjects[projectIndex].tasks = newTasks
-  return newProjects
-}
 
 const patchedTask = (responseTask, currentUser) => {
   let newProjects = [...currentUser.projects]
@@ -27,18 +12,6 @@ const patchedTask = (responseTask, currentUser) => {
   return newProjects
 }
 
-const patchedProject = (responseProject, currentUser) => {
-  let newProjects = [...currentUser.projects]
-  let projectIndex = newProjects.findIndex(p => p.id === responseProject.id)
-
-  newProjects[projectIndex] = responseProject
-  return newProjects
-}
-
-const postedProject = (responseProject, currentUser) => {
-  return [...currentUser.projects, responseProject]
-}
-
 const postedTask = (responseTask, currentUser) => {
   let newProjects = [...currentUser.projects]
   let projectIndex = newProjects.findIndex(
@@ -48,36 +21,6 @@ const postedTask = (responseTask, currentUser) => {
     ...newProjects[projectIndex].tasks,
     responseTask
   ]
-  return newProjects
-}
-
-const postedStep = (responseStep, currentUser) => {
-  let newProjects = [...currentUser.projects]
-  let projectIndex = newProjects.findIndex(
-    p => p.id === responseStep.task.project.id
-  )
-  let newTasks = [...newProjects[projectIndex].tasks]
-  let taskIndex = newTasks.findIndex(t => t.id === responseStep.task.id)
-  let newSteps = [...newTasks[taskIndex].steps, responseStep]
-
-  newTasks[taskIndex].steps = newSteps
-  newProjects[projectIndex].tasks = newTasks
-  return newProjects
-}
-
-const destroyedStep = (givenStep, currentUser) => {
-  let newProjects = [...currentUser.projects]
-  let projectIndex = newProjects.findIndex(
-    p => p.id === givenStep.task.project.id
-  )
-  let newTasks = [...newProjects[projectIndex].tasks]
-  let taskIndex = newTasks.findIndex(t => t.id === givenStep.task.id)
-  let newSteps = [...newTasks[taskIndex].steps]
-  let stepIndex = newSteps.findIndex(s => s.id === givenStep.id)
-
-  newSteps.splice(stepIndex, 1)
-  newTasks[taskIndex].steps = newSteps
-  newProjects[projectIndex].tasks = newTasks
   return newProjects
 }
 
@@ -92,35 +35,5 @@ const destroyedTask = (givenTask, currentUser) => {
   return newProjects
 }
 
-const destroyedProject = (givenProject, currentUser) => {
-  let newProjects = [...currentUser.projects]
-  let projectIndex = newProjects.findIndex(p => p.id === givenProject.id)
 
-  newProjects.splice(projectIndex, 1)
-  return newProjects
-}
-
-const postedProjectTaskStep = (
-  givenProject,
-  givenTask,
-  givenStep,
-  currentUser
-) => {
-  return [
-    ...currentUser.projects,
-    { ...givenProject, tasks: [{ ...givenTask, steps: [givenStep] }] }
-  ]
-}
-
-export default {
-  patchedStep,
-  postedStep,
-  destroyedStep,
-  patchedTask,
-  postedTask,
-  destroyedTask,
-  patchedProject,
-  postedProject,
-  destroyedProject,
-  postedProjectTaskStep
-}
+export default { patchedTask, postedTask, destroyedTask }
