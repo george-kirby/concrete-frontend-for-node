3c35b100c3c961f69944e051eb3712cc09e05962
george-kirby <georgeskirby@btinternet.com>
2019-11-13 10:34:34 +0000

    user guidance strings

---
+++ src/helpers/UserGuidance.js
@@ -0,0 +1,7 @@
+const UserGuidance = {
+    concreteStep: "Imagining a vivid first step makes you more likely to start your task.",
+    actionTime: "Setting an action time is better than deciding to start 'someday'.",
+    cue: "Picture a cue that will trigger you to start this task. ", 
+}
+
+export default UserGuidance
